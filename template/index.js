4241e260153eb530d2c5d06d596d2ce7021090fd
colin <colin@thinkr.fr>
2024-02-29 22:12:04 +0100

    feat: template update

--- template/index.js
+++ template/index.js
@@ -1,6 +1,9 @@
 const path = require('path');
 const { WebR } = require('webr');
-const { loadPackages } = require('spidyr');
+const { loadPackages, LibraryFromLocalFolder } = require('spidyr');
+
+const rfuns = new LibraryFromLocalFolder("rfuns");
+
 
 (async () => {
 
@@ -9,10 +12,20 @@ const { loadPackages } = require('spidyr');
   await globalThis.webR.init();
 
   console.log("👉 Loading R packages ----");
+
   await loadPackages(
     globalThis.webR,
     path.join(__dirname, 'webr_packages')
   )
+
+  await rfuns.mountAndLoad(
+    globalThis.webR,
+    path.join(__dirname, 'rfuns')
+  );
+
+  const hw = await rfuns.hello_world()
+
+  console.log(hw.values);
 
   console.log("✅ Everything is ready!");
 
