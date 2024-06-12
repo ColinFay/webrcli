f488329a5835be91a01ea83bea82383705127d8f
colin <colin@thinkr.fr>
2024-06-12 17:06:03 +0200

    feat: installFromPackageJson fails gracefully

--- tests/install.test.js
+++ tests/install.test.js
@@ -4,7 +4,6 @@ const { installIt, installFromDesc } = require('../src/install');
 const path = require('path');
 const os = require('os');
 const fs = require('fs');
-const { timeLog } = require('console');
 
 test('installIt works', async () => {
   let temp_dir = path.join(os.tmpdir(), 'install');
@@ -22,7 +21,7 @@ test('installIt works', async () => {
 
   expect(pkg_not_installed).toBe(false);
 
-  await installIt("attempt",temp_dir);
+  await installIt("attempt", temp_dir);
 
   expect(
     fs.existsSync(
@@ -134,3 +133,104 @@ test('installFromDesc works', async () => {
 
 }, 1000000);
 
+// test installFromPackageJson
+const { installFromPackageJson } = require('../src/installFromPackagejson.js');
+const { execSync } = require('child_process');
+
+test('installFromPackageJson works', async () => {
+  let temp_dir = path.join(os.tmpdir(), 'installFromPackageJson');
+  let temp_dir_dest = path.join(os.tmpdir(), 'installFromPackageJson_dest');
+
+  if (
+    fs.existsSync(temp_dir)
+  ) {
+    fs.rmdirSync(
+      temp_dir,
+      { recursive: true, force: true }
+    )
+  };
+
+  if (
+    fs.existsSync(temp_dir_dest)
+  ) {
+    fs.rmdirSync(
+      temp_dir_dest,
+      { recursive: true, force: true }
+    )
+  };
+
+  fs.mkdirSync(temp_dir);
+
+  const package_json = path.join(temp_dir, "package.json");
+
+  const package_not_installed = await installFromPackageJson(
+    package_json,
+    temp_dir_dest
+  );
+
+  expect(
+    package_not_installed
+  ).toBe(false);
+
+  fs.writeFileSync(
+    package_json,
+    JSON.stringify({
+      rdependencies: ["attempt", "rlang"]
+    })
+  );
+
+  await installFromPackageJson(
+    package_json,
+    temp_dir_dest
+  );
+
+  expect(
+    package_not_installed
+  ).toBe(false);
+
+  fs.mkdirSync(temp_dir_dest);
+
+  await installFromPackageJson(
+    package_json,
+    temp_dir_dest
+  );
+
+  expect(
+    fs.existsSync(
+      path.join(temp_dir_dest, 'attempt', 'DESCRIPTION')
+    )
+  ).toBe(true);
+
+  expect(
+    fs.existsSync(
+      path.join(temp_dir_dest, 'attempt', 'R')
+    )
+  ).toBe(true);
+
+  expect(
+    fs.existsSync(
+      path.join(temp_dir_dest, 'rlang')
+    )
+  ).toBe(true);
+
+  expect(
+    fs.existsSync(
+      path.join(temp_dir_dest, 'rlang', 'DESCRIPTION')
+    )
+  ).toBe(true);
+
+  expect(
+    fs.existsSync(
+      path.join(temp_dir_dest, 'rlang', 'R')
+    )
+  ).toBe(true);
+
+  fs.rmdirSync(
+    temp_dir,
+    { recursive: true, force: true }
+  );
+  fs.rmdirSync(
+    temp_dir_dest,
+    { recursive: true, force: true }
+  );
+})
